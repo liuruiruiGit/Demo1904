9ddb30c177ac43bffaab2b9c84bc8dd7c0cb18ac
3248436163@qq.com <3248436163@qq.com>
2021-12-08 15:26:11 +0800

    1

--- src/MyProject.Domain/Shopping/GoodsTypeModel.cs
+++ src/MyProject.Domain/Shopping/GoodsTypeModel.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
 
 namespace MyProject.Shopping
 {
-    public  class GoodsTypeModel
+    public  class GoodsTypeModel : AuditedAggregateRoot<Guid>
     {
         public Guid GoodId { get; set; }//商品id
         public Guid TypeId { get; set; }//类型id
